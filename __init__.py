8e9b3d84f3717652f1c08681d9f831a10f2210b7
Nynxz <contact@nynxz.com>
2025-11-26 21:32:37 +1000

    add entry point

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from comfy_api.latest import ComfyExtension, io
+from .nodes.CreateFadeCompareVideo import CreateFadeCompareVideo
+from .nodes.CreateWipeCompareVideo import CreateWipeCompareVideo
+
+
+class NynxzCustomNodesExtension(ComfyExtension):
+    async def get_node_list(self) -> list[type[io.ComfyNode]]:
+        return [CreateFadeCompareVideo, CreateWipeCompareVideo]
+
+
+async def comfy_entrypoint() -> ComfyExtension:
+    return NynxzCustomNodesExtension()
